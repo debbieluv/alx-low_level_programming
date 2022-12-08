4b44fd760a3687ba1e669755f956006647cc28ef
Deborah <deborahoigocho@gmail.com>
2022-12-08 14:14:31 -0800

    Wow

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
-int main(void){
+
+/**
+ *
+ *  * main - Entry point where actual printing starts
+ *
+ *   *
+ *
+ *    * Return: Always 0 if Successful
+ *
+ *     */
+
+int main (void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
-int main(void){
+
+/**
+ *
+ *  * main - Entry point where actual printing starts
+ *
+ *   *
+ *
+ *    * Return: Always 0 if Successful
+ *
+ *     */
+
+int main (void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
