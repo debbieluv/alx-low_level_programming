ca838a4f27b7f559daa37ec55f8eeb6177634922
Deborah <deborahoigocho@gmail.com>
2022-12-08 14:43:59 -0800

    Wow

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
-int main(void){
+
+/**
+ * main - main
+ *
+ * * Return: 0 if success
+ */
+
+int main(void)
+{
 	char achar;
 	int inte;
 	long onelong;
 	long long longlong;
 	float floater;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-	return(0);
+	printf("Size of a char: %lu byte(s)\n", sizeof(achar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(inte));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(onelong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longlong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floater));
+	return (0);
 }
