64df37b5ae45b1d171483ad64c16422cc0bb9e7b
Deborah <deborahoigocho@gmail.com>
2022-12-14 18:08:25 -0800

    functions - nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,10 @@ void print_alphabet_x10(void)
 {
 	char counter;
 
-	for (counter = 0; counter <= 10; counter++)
+	for (counter = 0; counter < 10; counter++)
 	{
 		char alpha;
+
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
