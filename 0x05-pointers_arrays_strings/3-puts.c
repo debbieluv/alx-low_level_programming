d2c105175ed24a2213a4bb55fee0de5d01fb11ff
Deborah <deborahoigocho@gmail.com>
2022-12-19 16:58:45 -0800

    Pointers_Arrays_Strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _puts: loop of string print
+*
+* Description: print strin
+* g
+*@str: string
+*/
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n')'
+}
