c5ad60522f3b60ef3849ab361de94c3ea5e2326e
Deborah <deborahoigocho@gmail.com>
2022-12-23 13:45:30 -0800

    Pointers and Arrays 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*_strncpy - mian
+* @src: string
+* @dest: string
+* @n: integer
+* Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
